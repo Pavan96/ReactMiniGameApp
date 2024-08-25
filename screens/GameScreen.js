22016250c6c6e56fd8fb58bd19e6ee32ee2948a5
Pavan <pavan.more1@gmail.com>
2024-08-26 00:11:28 +0530

    SafeAreaView added to add top margin

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,24 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet} from "react-native";
 
 function GameScreen() {
-    return <Text>Game screen!</Text>
+    return <View style={styles.screen}>
+      <Text> Oppenents Guest</Text> 
+       {/* Guess */}
+      <View>
+      <Text>Higher or lower?</Text>
+      {/*+ - */}
+      </View>
+      {/*<View>LOG ROUND</View> */}
+    </View>
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create ({
+ 
+screen: {
+    flex: 1,
+    padding: 12
+
+}
+});
