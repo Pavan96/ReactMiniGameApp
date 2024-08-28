d120a31185b0709273c516a64fd62c6f53a0bdfc
Pavan <pavan.more1@gmail.com>
2024-08-29 00:00:45 +0530

    Update UI

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,15 @@
 
 import { View, Image, StyleSheet } from "react-native";
 import Title from "../components/ui/Title";
+import Colors from "../constannts/colors";
 
 function GameOverScreen() {
  return (
     <View> 
         <Title>GAME OVER!</Title>
-        <Image source={require('../assets/images/success.png')} />
+        <View>
+           <Image source={require('../assets/images/success.png')} />
+        </View>
     </View>
     );
 }
@@ -14,6 +17,19 @@ function GameOverScreen() {
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
+ imageContainer: {
+    width: 400,
+    height: 400,
+    borderWidth: 3,
+    borderRadius: 200,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36
+ },
 
+ image: {
+    width: "100%",
+    height: "100%"
+ }
 
 });
